3921b66ea08baa89ff876366f3344e04549dbfdb
Marcos F3rnando Morales <125113310+Marcos-F-Morales@users.noreply.github.com>
2024-09-09 15:14:24 -0600

    manifest

--- web_app_psico_2024_backend/settings.py
+++ web_app_psico_2024_backend/settings.py
@@ -152,7 +152,7 @@ STATIC_URL = 'static/'
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE= 'whitenoise.storage.CompressedMainfestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
